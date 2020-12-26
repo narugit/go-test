79b87aec234d3ca7a00bc9480d4b386c459f4a2f
narugit <narusens@gmail.com>
2020-12-26 15:11:18 +0900

    test

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go-test
 
 go 1.15
 
-require github.com/narugit/go-test v0.0.0-20201226023231-519850c1e304 // indirect
+require github.com/nsf/termbox-go v0.0.0-20201124104050-ed494de23a00
--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 import (
-	mm "github.com/narugit/go-test/mymod/io"
+	mm "go-test/mymod/io"
+	"github.com/nsf/termbox-go"
 )
 
 
 func main() {
 	fmt.Printf("Hello World\n")
 	mm.DoSome()
+  if err := termbox.Init(); err != nil {
+        fmt.Printf("error")
+  }
+  defer termbox.Close()
 }
